f37e10db6adf817bcff1450d2b2ffd63b0f52aa5
Alejandro-Moles <amoleshurtado@gmail.com>
2023-03-17 10:17:13 +0100

    v2

--- Assets/Scripts/ControllersBehaviour.cs
+++ Assets/Scripts/ControllersBehaviour.cs
@@ -9,7 +9,8 @@ public class ControllersBehaviour : MonoBehaviour
     private bool leftPrimaryTouch;
     private bool rightPrimaryTouch;
 
-    // Start is called before the first frame update
+    public TextMeshProUGUI Pantalla_2;
+    
     void Start()
     {
         //Aqui cogemos los dipositivos y los guardamos en una lista
@@ -17,18 +18,12 @@ public class ControllersBehaviour : MonoBehaviour
         UnityEngine.XR.InputDevices.GetDevices(devices);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void MuestraDipositivos()
     {
         //Muestra todas las caracteristicas de todos los dipositivos
         foreach (var device in devices)
         {
-            Debug.Log(string.Format("Device name '{0}' has characteristics '{1}'", device.name, device.characteristics.ToString()));
+            Debug.Log(string.Format("Device name w'{0}' has characteristics '{1}'", device.name, device.characteristics.ToString()));
         }
     }
 
@@ -43,5 +38,22 @@ public class ControllersBehaviour : MonoBehaviour
     {
         Debug.Log("Izquierdo:" + leftPrimaryTouch);
         Debug.Log("Derecho: " + rightPrimaryTouch);
+    }
+
+
+    public void Button_Tablet_2(string texto)
+    {
+        StartCoroutine(EscribeTexto(texto));
+    }
+
+    private IEnumerator EscribeTexto(string frase)
+    {
+        Pantalla_2.text = "";
+
+        foreach(char character in frase)
+        {
+            Pantalla_2.text = Pantalla_2.text + character;
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
